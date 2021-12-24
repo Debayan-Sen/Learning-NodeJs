a63bea2b7a0794e461a5492a13538a9351c53fed
Debayan-Sen <sdebayan999@gmail.com>
2021-12-24 23:37:33 +0530

    done with file upload

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
-require('./config');
-const Building = require('./building');
-
+const multer = require('multer');
 const app = express();
-app.use(express.json());
 
-app.get("/search/:key", async (req, res) => {
-    console.log(req.params.key);
-    let data = await Building.find({
-        "$or":[
-            {"name":{$regex:req.params.key}},
-            {"type":{$regex:req.params.key}}
-        ]
-    });
-    res.send("search for " + req.params.key + "\n" + data);
-})
+const upload = multer({
+    storage:multer.diskStorage({
+        destination:function(req,file,cb){
+            cb(null,"uploads");
+        },
+        filename:function(req,file,cb){
+            cb(null,file.fieldname+"-"+Date.now()+".jpg")
+        }
+    })
+}).single("user_file");
 
-app.listen(7500);
+app.post("/upload", upload, (req, resp)=>{
+    resp.send("file upload");
+});
+
+app.listen(7000);
+
+//OS module
+const os = require('os');
+//console.log(os.arch());
+// console.log(os.freemem()/(1024*1024*1024));
+// console.log(os.totalmem()/(1024*1024*1024));
+// console.log(os.platform());
+console.log(os.userInfo());
